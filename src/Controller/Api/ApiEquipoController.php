cb17916ddf6cb2aae1f2911bfa12a1bc9918b94e
Yonti <desprogramaci@gmail.com>
2020-08-30 15:08:21 +0200

    Procesos en API

--- src/Controller/Api/ApiEquipoController.php
+++ src/Controller/Api/ApiEquipoController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+use App\Entity\Equipo;
+
 /**
  * @Route("/api/v1")
  */
@@ -14,21 +16,109 @@ class ApiEquipoController extends AbstractController
     /**
      * @Route("/equipo/create", name="api_equipo_create", methods={"POST"})
      */
-    public function api(Request $request)
+    public function create(Request $request)
     {
+
+        $em = $this->getDoctrine()->getManager();
         $nombre = $request->get('nombre');
+
+        $equipo_new = new Equipo();
+        $equipo_new->setNombre($nombre);
+        $em->persist($equipo_new);
+        $em->flush();
+        
         return $this->json([
-            'message' => "create new equipo $nombre"
+            'message' => "Nuevo Equipo Creado:  $nombre"
         ]);
     }
     /**
      * @Route("/equipo/all", name="api_equipo_all", methods={"GET"})
      */
-    public function list()
+    public function list_equipo()
     {
-
+       $em = $this->getDoctrine()->getManager();
+       $repositorio = $em->getRepository(Equipo::class);
+       $equipos = $repositorio->findAll();
+       
+       
+       $mensaje='No existen Posiciones Registradas';
+       
+        if(!empty($equipo)){
+            
+            $mensaje =$mensaje;
+            
+        }else{
+            
+            $mensaje =$equipos;
+            
+        }
+       
+       
         return $this->json([
-            'list' => 'listar equipo'
+            'lista' => $equipos
+        ]);
+    }
+    
+    
+        
+     /**
+     * @Route("/equipo/edit/{id}", name="api_equipo_edit", methods={"POST"})
+     */
+    public function edit($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repositorio = $em->getRepository(Equipo::class);
+       
+        $nombre = $request->get('nombre');
+        
+        $equipo = $repositorio->find($id);
+        
+        $mensaje = "Equipo Actualizado Corectamente!!";
+        if(!empty($equipo)){
+            
+            $equipo = $repositorio->find($id);
+            $equipo->setNombre($nombre);
+            $em->persist($equipo);
+            $em->flush();
+        
+            $mensaje =$mensaje;
+             
+        }else{
+            $mensaje='Equipo no Encontrado';
+        }
+ 
+       
+        return $this->json([
+            'message' => $mensaje
+        ]);
+    }
+    
+
+    
+     /**
+     * @Route("/equipo/delete/{id}", name="api_equipo_delete", methods={"GET"})
+     */
+    public function delete($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repositorio = $em->getRepository(Equipo::class);
+       
+        $equipo = $repositorio->find($id);
+        
+        $mensaje = "Equipo Eliminado Corectamente!!";
+        if(!empty($equipo)){
+            
+             $em->remove($equipo);
+             $em->flush();
+             $mensaje =$mensaje;
+             
+        }else{
+            $mensaje='Equipo no Encontrado';
+        }
+ 
+       
+        return $this->json([
+            'message' => $mensaje
         ]);
     }
 }
--- src/Controller/Api/ApiPosicionController.php
+++ src/Controller/Api/ApiPosicionController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Posicion;
 
 /**
@@ -14,24 +15,110 @@ class ApiPosicionController extends AbstractController
     /**
      * @Route("/posicion/create", name="api_posicion_create", methods={"POST"})
      */
-    public function api()
+    public function create(Request $request)
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $nombre = $request->get('nombre');
+
+        $posicion_new = new Posicion();
+        $posicion_new->setNombre($nombre);
+        $em->persist($posicion_new);
+        $em->flush();
         
         return $this->json([
-            'message' => 'create new posicion'
+            'message' => "Nueva Posicion Creada:  $nombre"
         ]);
     }
+
     /**
      * @Route("/posicion/all", name="api_posicion_all", methods={"GET"})
      */
-    public function list()
+    public function list_posicion()
     {
        $em = $this->getDoctrine()->getManager();
        $repositorio = $em->getRepository(Posicion::class);
-       $posiciones = $repositorio->findAll();
+       $posicion = $repositorio->findAll();
+       
+       $mensaje='No existen Posiciones Registradas';
+       
+        if(!empty($equipo)){
+            
+            $mensaje =$mensaje;
+            
+        }else{
+            
+            $mensaje =$posicion;
+            
+        }
+        
        
         return $this->json([
-            'Posiciones' => $posiciones
+            'listado' => $mensaje
         ]);
     }
+    
+    
+    
+    /**
+     * @Route("/posicion/edit/{id}", name="api_posicion_edit", methods={"POST"})
+     */
+    public function edit($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repositorio = $em->getRepository(Posicion::class);
+       
+        $nombre = $request->get('nombre');
+        
+        $posicion_all = $repositorio->find($id);
+        
+        $mensaje = "Posicion Actualizada Corectamente!!";
+        if(!empty($posicion_all)){
+
+            $posicion_all->setNombre($nombre);
+            $em->persist($posicion_all);
+            $em->flush();
+        
+            $mensaje =$mensaje;
+             
+        }else{
+            $mensaje='Posicion no Encontrada';
+        }
+ 
+       
+        return $this->json([
+            'message' => $mensaje
+        ]);
+    }
+    
+    
+    /**
+     * @Route("/posicion/delete/{id}", name="api_posicion_delete", methods={"GET"})
+     */
+    public function delete($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repositorio = $em->getRepository(Posicion::class);
+        
+        $posicion = $repositorio->find($id);
+        
+        $mensaje = "Posicion Eliminada Corectamente!!";
+        if(!empty($posicion)){
+            
+             $em->remove($posicion);
+             $em->flush();
+             $mensaje =$mensaje;
+             
+        }else{
+            $mensaje='Posicion no Encontrada';
+        }
+ 
+       
+        return $this->json([
+            'message' => $mensaje
+        ]);
+    }
+    
+    
+    
 }
